0c397ba4fd04a05208c4e7fe5ee3de9cd398c996
nickblack647 <nickblack647@gmail.com>
2019-09-27 11:33:01 -0700

    Added timer

--- js/main.js
+++ js/main.js
@@ -93,6 +93,44 @@ var UIController = (function() {
             } else {
                 document.querySelector('.results-header').textContent = 'You\'re metal!';
             }
+        },
+        startTimer: function() {
+            // 10 minute countdown
+            var time_in_minutes = 10;
+            var current_time = Date.parse(new Date());
+            var deadline = new Date(current_time + time_in_minutes*60*1000);
+
+            function time_remaining(endtime){
+                var t = Date.parse(endtime) - Date.parse(new Date());
+                var seconds = Math.floor( (t/1000) % 60 ).toLocaleString(undefined,{minimumIntegerDigits: 2});
+                var minutes = Math.floor( (t/1000/60) % 60 ).toLocaleString(undefined,{minimumIntegerDigits: 2});
+                var hours = Math.floor( (t/(1000*60*60)) % 24 );
+                var days = Math.floor( t/(1000*60*60*24) );
+                return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
+            }
+
+            function run_clock(id,endtime){
+                var clock = document.getElementById(id);
+                function update_clock(){
+                    var t = time_remaining(endtime);
+                    clock.innerHTML = t.minutes + ':' + t.seconds;
+                    if(t.total<=0){ clearInterval(timeinterval); }
+                }
+                update_clock(); // run function once at first to avoid delay
+                var timeinterval = setInterval(update_clock,1000);
+            }
+            run_clock(DOMstrings.timer,deadline);
+
+            setInterval(function() {
+                var time = document.getElementById(DOMstrings.timer).textContent;
+                if(time == '0:00') {
+                    document.getElementById(DOMstrings.quiz).style.display = 'none';
+                    document.getElementById(DOMstrings.scoreBar).style.opacity = 0;
+                    document.getElementById(DOMstrings.results).style.display = 'block';
+                    document.querySelector('.results-header').textContent = 'You ran out of time!';
+                    document.getElementById(DOMstrings.finalScore).style.display = 'none';
+                }
+            },1000);
         }
     };
 
@@ -115,6 +153,7 @@ var controller = (function(questionsCtrl, UICtrl) {
             UICtrl.showQuiz();
             questionsCtrl.loadQuestion(questionNumber);
             document.getElementById('start').style.display = 'none';
+            UICtrl.startTimer();
         });
 
         // Check if selected option is correct and change questions
